4e642c6581dc14546b5a0ba813c58033c7407a7c
Yusuf <yusufcakal77@gmail.com>
2022-01-18 11:23:01 +0300

    Added No Use Case Handler
    it's been added the no param use case handler to be able to handle without any use case and return a response model

---
+++ payment-api/domain/src/main/java/com/hexagonaldemo/paymentapi/common/usecase/NoUseCaseHandler.java
@@ -0,0 +1,6 @@
+package com.hexagonaldemo.paymentapi.common.usecase;
+
+public interface NoUseCaseHandler<R> {
+
+    R handle();
+}
--- payment-api/domain/src/main/java/com/hexagonaldemo/paymentapi/common/usecase/UseCaseHandlerRegistry.java
+++ payment-api/domain/src/main/java/com/hexagonaldemo/paymentapi/common/usecase/UseCaseHandlerRegistry.java
@@ -13,12 +13,14 @@ public class UseCaseHandlerRegistry {
 
     private Map<Class<? extends UseCase>, UseCaseHandler<?, ? extends UseCase>> registryForUseCaseHandlers;
     private Map<Class<? extends UseCase>, VoidUseCaseHandler<? extends UseCase>> registryForVoidUseCaseHandlers;
+    private Map<Class<?>, NoUseCaseHandler<?>> registryForNoUseCaseHandlers;
 
     public static final UseCaseHandlerRegistry INSTANCE = new UseCaseHandlerRegistry();
 
     private UseCaseHandlerRegistry() {
         registryForUseCaseHandlers = new HashMap<>();
         registryForVoidUseCaseHandlers = new HashMap<>();
+        registryForNoUseCaseHandlers = new HashMap<>();
     }
 
     public <R, T extends UseCase> void register(Class<T> key, UseCaseHandler<R, T> useCaseHandler) {
@@ -31,11 +33,20 @@ public class UseCaseHandlerRegistry {
         registryForVoidUseCaseHandlers.put(key, useCaseHandler);
     }
 
+    public <R> void register(Class<R> key, NoUseCaseHandler<R> useCaseHandler) {
+        log.info("Use case {} is registered by no param handler {}", key.getSimpleName(), useCaseHandler.getClass().getSimpleName());
+        registryForNoUseCaseHandlers.put(key, useCaseHandler);
+    }
+
     public UseCaseHandler<?, ? extends UseCase> detectUseCaseHandlerFrom(Class<? extends UseCase> useCaseClass) {
         return registryForUseCaseHandlers.get(useCaseClass);
     }
 
     public VoidUseCaseHandler<? extends UseCase> detectVoidUseCaseHandlerFrom(Class<? extends UseCase> useCaseClass) {
         return registryForVoidUseCaseHandlers.get(useCaseClass);
+    }
+
+    public NoUseCaseHandler<?> detectNoUseCaseHandlerFrom(Class<?> returnClass) {
+        return registryForNoUseCaseHandlers.get(returnClass);
     }
 }
